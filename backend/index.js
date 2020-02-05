1b7dc58a05d9a8f2cbc9e7f121f585f52e59cfb4
Helton Mori <heltonmori@gmail.com>
2020-02-04 22:03:45 -0300

    Iniciando conexão com o mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-vckml.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
