ed503c2dc03f674d116faad24d93eb8c8e8aea83
Helton Mori <heltonmori@gmail.com>
2020-02-17 22:19:24 -0300

    Componentização do formulário e listas

--- web/src/App.js
+++ web/src/App.js
@@ -1,84 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+
+    const response = await api.post('/devs', data)
+    
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div class="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-          </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>     
       </aside>
       <main>
         <ul>
-        <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/12644707?v=4" alt="Helton"/>
-                <div className="user-info">
-                  <strong>Helton</strong>
-                  <span>React, React Native</span>
-                  <p>Dev na advise</p>
-                  <a href="https://github.com/helton-mori-dev">Acessar perfil do github</a>
-                </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/12644707?v=4" alt="Helton"/>
-                <div className="user-info">
-                  <strong>Helton</strong>
-                  <span>React, React Native</span>
-                  <p>Dev na advise</p>
-                  <a href="https://github.com/helton-mori-dev">Acessar perfil do github</a>
-                </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/12644707?v=4" alt="Helton"/>
-                <div className="user-info">
-                  <strong>Helton</strong>
-                  <span>React, React Native</span>
-                  <p>Dev na advise</p>
-                  <a href="https://github.com/helton-mori-dev">Acessar perfil do github</a>
-                </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/12644707?v=4" alt="Helton"/>
-                <div className="user-info">
-                  <strong>Helton</strong>
-                  <span>React, React Native</span>
-                  <p>Dev na advise</p>
-                  <a href="https://github.com/helton-mori-dev">Acessar perfil do github</a>
-                </div>
-            </header>
-          </li>
+          { devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
